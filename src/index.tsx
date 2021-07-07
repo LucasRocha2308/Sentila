a0ea5bd8170f10e794ff17bccfd73ade87245d67
Itallo <dornelaszxd@gmail.com>
2021-07-07 11:44:50 -0300

    feat: provider estruturado

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyled from "../src/styles/global";
+import Provider from "./provider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyled />
-    <App />
+    <Provider>
+      <GlobalStyled />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/provider/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+const Providers = ({ children }: ProvidersProps) => {
+  return <>{children}</>;
+};
+
+export default Providers;
