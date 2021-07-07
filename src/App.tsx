4d9954c23b39970339737d8cb2d5cfcd77a26c75
joao alves da silva junior <kanonxp@gmail.com>
2021-07-07 18:09:30 -0300

    feat: grafico criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import MoreSpent from "./components/MoreSpent";
+import MoreSaved from "./components/WhoSavedTheMost";
 
 function App() {
   return (
     <>
       <MoreSpent />
+      <MoreSaved />
     </>
   );
 }
