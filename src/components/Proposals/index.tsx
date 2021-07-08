70abbf097eb7bea6c1c6c2b1a84092b67015cf66
Itallo <dornelaszxd@gmail.com>
2021-07-08 10:40:30 -0300

    feat: iniciando componente de propostas

---
+++ src/components/Proposals/index.tsx
@@ -0,0 +1,10 @@
+import { ProposalsContainer } from "./styles";
+
+const Proposals = () => {
+  return (
+    <ProposalsContainer>
+      <h2>Propostas 📝</h2>
+    </ProposalsContainer>
+  );
+};
+export default Proposals;
