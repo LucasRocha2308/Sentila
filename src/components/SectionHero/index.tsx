accd5d96c13f7d779e465d7c55f7c513a90d3146
Itallo <dornelaszxd@gmail.com>
2021-07-12 18:13:28 -0300

    feat: novos ajustes na pagina de proposta

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -2,19 +2,18 @@ import { Container } from "./styles";
 import { ImPlay2 } from "react-icons/im";
 
 const SectionHero = () => {
-    return (
-        <Container>
-            <h1>APLICATIVO</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna sed do
-                eiusmo daliqua.
-            </p>
-            <a href="https">
-                Saiba mais <ImPlay2 />
-            </a>
-        </Container>
-    );
+  return (
+    <Container>
+      <h1>APLICATIVO</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna sed do eiusmo daliqua.
+      </p>
+      <a href="/about">
+        Saiba mais <ImPlay2 />
+      </a>
+    </Container>
+  );
 };
 
 export default SectionHero;
