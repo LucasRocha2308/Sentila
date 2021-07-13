c364246f56583c7a71ea145985bca477772bd8bc
Ioane <johnfaumui@gmail.com>
2021-07-12 22:13:49 -0300

    fix: ajuste em rotas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Expenses from "../pages/Expenses";
+import Despesas from "../pages/Despesas";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -13,7 +13,7 @@ function AppRoutes() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/expenses" component={Expenses} />
+        <Route exact path="/despesas" component={Despesas} />
         <Route exact path="/proposals" component={Home} />
         <Route exact path="/about" component={Home} />
       </Switch>
