9d91003d3a6f185f1144763de425e1ee2147e34c
joao alves da silva junior <kanonxp@gmail.com>
2021-07-14 12:03:03 -0300

    feat: botao para enviar email criado

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -2,7 +2,7 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Collapse from "@material-ui/core/Collapse";
 
 import { useState } from "react";
-import { ContentStyled, IconButtonStyled } from "./styles";
+import { ContentStyled, IconButtonStyled, ButtonEmailStyled } from "./styles";
 import { useIsLogin } from "../../provider/isLogin";
 
 const Contacts = ({ data }: any) => {
@@ -28,14 +28,18 @@ const Contacts = ({ data }: any) => {
             </IconButtonStyled>
             <Collapse in={expandedNav} timeout="auto" unmountOnExit>
               <nav>
-                <p>
-                  <span>Telefone :</span> {data.telefone}
-                </p>
-                <p>
+                <div>
+                  <span>Telefone :</span>
+                  <p>(61) {data.telefone}</p>
+                </div>
+                <div>
                   <span>E-mail : </span>
-                  {data.email}
-                </p>
+                  <p>{data.email}</p>
+                </div>
               </nav>
+              <ButtonEmailStyled href={`mailto:${data.email}`}>
+                Enviar email
+              </ButtonEmailStyled>
             </Collapse>
           </ContentStyled>
         )}
