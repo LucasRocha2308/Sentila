5c1cf16f1da34fd6f236b744c1a7095167cec950
Amanda Medeiros <medeiros.amandas@gmail.com>
2021-07-14 01:20:47 -0300

    fix: increasing the size of the inputs

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import { TextField } from "@material-ui/core";
 
 export const Container = styled.div`
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  /* div {
+    /* div {
     span {
       color: var(--red);
     }
@@ -18,25 +18,25 @@ export const Container = styled.div`
 //   isErrored: boolean;
 // }
 export const InputContainer = styled.div`
-  /* transition: 0.4s; */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* position: relative; */
+    /* transition: 0.4s; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* position: relative; */
 `;
 
 export const StyleTextField = styled(TextField)`
-  label.Mui-focused {
-    color: var(--light-blue);
-  }
-
-  .MuiOutlinedInput-root {
-    margin-bottom: 45px;
-    padding: 2px 120px 2px 5px;
-
-    /* width: 300px; */
-    &.Mui-focused fieldset {
-      border-color: var(--light-blue);
+    label.Mui-focused {
+        color: var(--light-blue);
     }
-  }
+
+    .MuiOutlinedInput-root {
+        margin-bottom: 45px;
+        padding: 2px;
+
+        /* width: 300px; */
+        &.Mui-focused fieldset {
+            border-color: var(--light-blue);
+        }
+    }
 `;
